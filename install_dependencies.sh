911bda6a03645f0ce9f4a0a6dad27ddcf86f928e
LaViAn-M-Et-H <susanomemia@gmail.com>
2025-04-01 18:28:09 +0700

    Add files via upload

---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# File: install_dependencies.sh
+sudo apt-get install -y \
+ros-noetic-robot-state-publisher \
+ros-noetic-joint-state-publisher-gui \
+ros-noetic-gazebo-ros \
+ros-noetic-controller-manager \
+ros-noetic-joint-state-controller \
+ros-noetic-position-controllers \
+ros-noetic-effort-controllers
